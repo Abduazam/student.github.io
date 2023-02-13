341ed96cf0624887dbfc43595bb53e88ab52b920
ZoxidovSunnatilla <sunnatzoxidov3@gmail.com>
2023-02-13 18:06:27 +0300

    Sunnat-12

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,44 +1,3 @@
-const nextIcon = '<img src="./assets/images/left_icon2.svg" alt="right">';
-const prevIcon = '<img src="./assets/images/right_icon2.svg" alt="left">';
-$('.team .owl-carousel').owlCarousel({
-    loop:true,
-    autoplay: true,
-    nav: true,
-    navText: [
-    prevIcon,
-    nextIcon
-  ],
-    responsive:{
-        0:{
-            items:4
-        },
-        1000:{
-            items:3
-        },
-        1500:{
-            items:4
-        }
-    }
-  })
-
-// SELECT SETTINGS
-const selectBoxCurrent = document.querySelector(".select-box__current");
-const selectBoxList = document.querySelector(".select-box__list");
-const selectBoxIcon = document.querySelector(".select-box__icon");
-let k = true;
-selectBoxCurrent.addEventListener("click", (e) => {
-    if(k === false) {
-        selectBoxList.style.opacity = "0";
-        selectBoxIcon.style.transform = "translateY(-50%) rotate(0deg)";
-        k = true;
-    } else {
-        selectBoxIcon.style.transform = "translateY(-50%) rotate(180deg)";
-        selectBoxList.style.opacity = "1";
-        k = false;
-    }
-})
-
-
 let rangeMin = 100;
 const range = document.querySelector(".range-selected");
 const rangeInput = document.querySelectorAll(".range-input input");
@@ -78,6 +37,26 @@ rangePrice.forEach((input) => {
         }
     });
 });
+
+// SELECT SETTINGS
+const selectBoxCurrent = document.querySelector(".select-box__current");
+const selectBoxList = document.querySelector(".select-box__list");
+const selectBoxIcon = document.querySelector(".select-box__icon");
+let k = true;
+selectBoxCurrent.addEventListener("click", (e) => {
+    if(k === false) {
+        selectBoxList.style.opacity = "0";
+        selectBoxIcon.style.transform = "translateY(-50%) rotate(0deg)";
+        k = true;
+    } else {
+        selectBoxIcon.style.transform = "translateY(-50%) rotate(180deg)";
+        selectBoxList.style.opacity = "1";
+        k = false;
+    }
+})
+
+
+
 let badge1 = document.querySelector(".header_badge1");
 let sear_ish = document.querySelector(".sear_ish");
 let btn_yonalish = document.querySelector(".btn_yonalish");
@@ -147,3 +126,25 @@ window.onscroll = function() {
 };
 
 
+const nextIcon = '<img src="./assets/images/left_icon2.svg" alt="right">';
+const prevIcon = '<img src="./assets/images/right_icon2.svg" alt="left">';
+$('.team .owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    nav: true,
+    navText: [
+    prevIcon,
+    nextIcon
+  ],
+    responsive:{
+        0:{
+            items:4
+        },
+        1000:{
+            items:3
+        },
+        1500:{
+            items:4
+        }
+    }
+  })
